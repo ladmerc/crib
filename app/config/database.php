6248eda16902ee777e12d753cb01770b1115d9bc
Meke Ladna <ladna_mekelive@yahoo.com>
2015-12-14 23:02:38 +0100

    Create initial migration for users

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'crib',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'Edulyn33',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
