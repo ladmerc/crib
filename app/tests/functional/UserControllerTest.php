557d35e742f84ed1ad0b4b9993bd91dbb44b1e9d
Meke Ladna <ladna_mekelive@yahoo.com>
2015-12-17 05:55:19 +0100

    create functional controller tests using mockery

---
+++ app/tests/functional/UserControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+class UserControllerTest extends TestCase
+{
+	public function setUp()
+	{
+	  parent::setUp();
+	 
+	  $this->mock = $this->mock('Crib\Storage\User\UserRepository');
+	}
+
+	public function tearDown()
+	{
+	  Mockery::close();
+	}
+	  
+	public function mock($class)
+	{
+	  $mock = Mockery::mock($class);
+	  
+	  $this->app->instance($class, $mock);
+	  
+	  return $mock;
+	}
+
+	// alias to run this test only by phpunit --group user
+	/**
+	* @group user
+	*/
+
+	public function testIndex()
+	{
+	  $this->mock->shouldReceive('all')->once();
+	 
+	  $this->call('GET', 'user');
+	 
+	  $this->assertResponseOk();
+	}
+
+	/**
+	* @group user2
+	*/
+
+	// public function testArchiveHasPosts()
+	// {
+	//   $this->mock
+	//     ->shouldReceive('all')
+	//     ->once()
+	//     ->andReturn('foo');
+	  
+	//   // $this->app->instance('Post', $this->mock);
+	  
+	//   $this->call('GET', 'user');
+	  
+	//   $this->assertViewHas('user');
+	// }
+}
